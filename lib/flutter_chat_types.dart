e58bd15fe4bad9d823b634f21fb600b6013dc290
Jordan de Vogelaere <email@jordandevogelaere.be>
2022-11-21 13:52:09 +0100

    fix exports

--- lib/flutter_chat_types.dart
+++ lib/flutter_chat_types.dart
@@ -1,9 +1,14 @@
 library flutter_chat_types;
 
+export 'src/channels.dart';
+export 'src/country.dart';
 export 'src/message.dart';
 export 'src/messages/partial_text.dart';
 export 'src/messages/system_message.dart';
 export 'src/messages/text_message.dart';
 export 'src/preview_data.dart';
 export 'src/preview_data.dart';
+export 'src/registered_channel.dart';
+export 'src/room.dart';
 export 'src/user.dart';
+export 'src/virtual_number.dart';
